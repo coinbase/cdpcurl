a4ff09cdb71bf6a04055427885e1dbfb51949776
Di Mei <di.mei@coinbase.com>
2024-05-16 11:24:50 -0400

    refactor

--- Formula/cdpcurl.rb
+++ Formula/cdpcurl.rb
@@ -1,7 +1,7 @@
 class Cdpcurl < Formula
   desc "curl with CDP API key"
   homepage "https://github.com/dimei-BT/cdpcurl"
-  url "https://github.com/coinbase/dimei-BT/cdpcurl/archive/cdpcurl-v0.0.1.tar.gz"
+  url "https://github.com/dimei-BT/cdpcurl/archive/cdpcurl-v0.0.1.tar.gz"
   sha256 "028e4a22226baae713232474f9c5ae02b361f9b63564ac3c838f8b53162cf176"
 
   def install
