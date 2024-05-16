2beb386750609ed0745c386f4afaa48c9aa32593
Di Mei <di.mei@coinbase.com>
2024-05-16 13:56:05 -0400

    add version number

--- Formula/cdpcurl.rb
+++ Formula/cdpcurl.rb
@@ -3,12 +3,15 @@ class Cdpcurl < Formula
   homepage "https://github.com/dimei-BT/cdpcurl"
   url "file:///Users/dimei/Downloads/cdpcurl-0.0.1.tar.gz"
   sha256 "a894b2f9bffa020b7cbfa877fed9aa25fb6529c85376590ce0eb421f4fcd40ed"
+  license "MIT"
+
+  depends_on "go" => :build
 
   def install
     system "go", "build", "-o", "#{bin}/cdpcurl", "."
   end
 
   test do
-    system "#{bin}/cdpcurl", "--version"
+    assert_match "cdpcurl version", shell_output("#{bin}/cdpcurl --version")
   end
 end
